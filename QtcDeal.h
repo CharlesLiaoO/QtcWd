0bcb62838288120ce8df2b700c4bf1626583494e
LiaoCheng <LiaoChengO@qq.com>
2024-08-02 18:44:18 +0800

    fix deletion of existing build directory

--- QtcDeal.h
+++ QtcDeal.h
@@ -6,6 +6,7 @@
 QString GetQtcShortcut(QString key);
 void CloseCurrentProject();
 void ModifyProjectConfig(QString projectDir);
+void ModifyProjectConfig_BD(const QString &projectDir, QString &sLine, int idxKey, const QString &ssBd);
 void ReopenProject(QString projectDir);
 
 #endif // QTCDEAL_H
